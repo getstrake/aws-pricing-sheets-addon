6e17d4e6dc430241ccca07cb86ee41d074675188
wimdenherder <wimdenherder@gmail.com>
2022-12-31 09:59:18 +0100

    added validation multiple or no prices found

--- src/Code.js
+++ src/Code.js
@@ -1,4 +1,4 @@
-const cacheLoader = new CacheLoader(CacheService.getScriptCache());
+const cache = new CacheLoader(CacheService.getScriptCache());
 
 function fetchApi(options) {
     const {instanceType, region, purchaseType, platform, offeringClass, purchaseTerm, paymentOption} = options;
@@ -6,6 +6,10 @@ function fetchApi(options) {
     const url = `${cfg.baseHost}${path}?timestamp=${Date.now()}`;
     const response = fetchUrlCached(url);
     const filteredPrices = filterPrices(response.prices, options);
+    if(filteredPrices.length === 0)
+      throw new Error(`No price found for ${JSON.stringify(options)}`);
+    if(filterPrices.length > 1)
+      throw new Error(`Multiple prices found for ${JSON.stringify(options)}`
     const price = purchaseType === "ondemand" ? 
       filteredPrices[0].price.USD : 
       filteredPrices[0].calculatedPrice.effectiveHourlyRate.USD
@@ -19,25 +23,25 @@ const filterPrices = (prices, options) => {
        'no_upfront': 'No Upfront',
        'partial_upfront': 'Partial Upfront'
     }
-    const filterReserved = price => 
-        price.attributes['aws:ec2:instanceType'] === instanceType &&
-        price.attributes['aws:offerTermOfferingClass'] === offeringClass &&
-        price.attributes['aws:offerTermPurchaseOption'] === paymentOptionLib[paymentOption] &&
-        price.attributes['aws:offerTermLeaseLength'] === purchaseTerm + 'yr';
-    const filterOnDemand = price => {
-        return price.attributes['aws:ec2:instanceType'] === instanceType;
-    }
+    const filterReserved = p => 
+        p.attributes['aws:ec2:instanceType'] === instanceType &&
+        p.attributes['aws:offerTermOfferingClass'] === offeringClass &&
+        p.attributes['aws:offerTermPurchaseOption'] === paymentOptionLib[paymentOption] &&
+        p.attributes['aws:offerTermLeaseLength'] === purchaseTerm + 'yr';
+    const filterOnDemand = p => 
+        p.attributes['aws:ec2:instanceType'] === instanceType;
+        
     return purchaseType === "ondemand" ? 
       prices.filter(filterOnDemand) : 
       prices.filter(filterReserved);
 }
 
 function fetchUrlCached(url) {
-    return cacheLoader.get(url) || cacheLoader.putAndReturn(url, fetchUrl(url));
+    return cache.get(url) || cache.putAndReturn(url, fetchUrl(url));
 }
 
 function fetchUrl(url) {
-    let resp = UrlFetchApp.fetch(url);
+    const resp = UrlFetchApp.fetch(url);
     if (resp.getResponseCode() != 200) throw "Unable to load the URL: " + url;
     return JSON.parse(resp.getContentText());
 }
