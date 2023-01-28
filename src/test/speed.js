d9aa40356520d8c794593a4e787b48d9e6961c7d
wimdenherder <wimdenherder@gmail.com>
2023-01-28 16:00:01 +0100

    Speed test with or without Google Analytics

---
+++ src/test/speed.js
@@ -0,0 +1,15 @@
+function speedComparisonWithOrWithoutAnalytics() {
+  const urlCDNPath = 'https://cdn.x.macroscope.io/aws-pricing/retro/pricing/1.0/rds/region/us-east-1/aurora/mysql/reserved-instance/single-az/index.json';
+  
+  cache.remove(urlCDNPath);
+  cfg.logCustomFunctionToAnalytics = true;
+  console.time('WITH_ANALYTICS')
+  console.log(AWS_RDS("aurora/mysql", "db.r6g.xlarge", "us-east-1", "reserved", "3yr", "partial_upfront"))
+  console.timeEnd('WITH_ANALYTICS')
+  
+  cache.remove(urlCDNPath);
+  cfg.logCustomFunctionToAnalytics = false;
+  console.time('WITHOUT_ANALYTICS')
+  console.log(AWS_RDS("aurora/mysql", "db.r6g.xlarge", "us-east-1", "reserved", "3yr", "partial_upfront"))
+  console.timeEnd('WITHOUT_ANALYTICS')
+}
