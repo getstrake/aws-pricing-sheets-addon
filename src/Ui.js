9e1a0d6a6929f4815e807303bbc28ffa2d9aa139
wimdenherder <wimdenherder@gmail.com>
2023-01-30 10:37:56 +0100

    Save user information when onboarding via menu

--- src/Ui.js
+++ src/Ui.js
@@ -1,6 +1,8 @@
-function showUi() {
+function onboarding() {
+  const {email, userLocale} = saveUserInformation();
+  createHubSpotContact({email, userLocale});
   const ui = SpreadsheetApp.getUi();
-  const template = HtmlService.createTemplateFromFile('Sidebar.html');
+  const template = HtmlService.createTemplateFromFile('src/Sidebar.html');
   const html = template.evaluate();
   html.setTitle('Insert formula');
   ui.showSidebar(html);
