a8daeed76ecb2289d1b8c4447b9cfff5b492c5d6
wimdenherder <wimdenherder@gmail.com>
2023-02-09 19:26:04 +0100

    Add collapsible chapters and logos in sidebar readme for onboarding users

--- src/Ui.js
+++ src/Ui.js
@@ -9,7 +9,7 @@ function onboarding() {
   const {email, userLocale} = saveUserInformation();
   createHubSpotContact({email, userLocale});
   const ui = SpreadsheetApp.getUi();
-  const template = HtmlService.createTemplateFromFile('help_dialog.html');
+  const template = HtmlService.createTemplateFromFile('help_dialog_collapsed.html');
   const html = template.evaluate();
   html.setTitle('AWS Pricing Add-on');
   ui.showSidebar(html);
