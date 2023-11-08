bedb2323c744fcfc733a264a756e076905e94f56
wimdenherder <wimdenherder@gmail.com>
2023-11-08 20:32:42 +0100

    Change CDN path to https://cdn.x.macroscope.io/aws-pricing/testing/to_prod

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 const cfg = {
-  baseHost: 'https://cdn.x.macroscope.io/aws-pricing/retro/pricing/1.2',
+  baseHost: 'https://cdn.x.macroscope.io/aws-pricing/testing/to_prod',
   hoursPerMonth: 730,
   logCustomFunctionToAnalytics: false, // analytics, requires credentials, otherwise skips
   environment: "production", // should be either "development" or "production"
